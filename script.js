50e17827bebcc8c21c400909f2d39367960e9735
sumita <sumitabalouch@gmail.com>
2024-06-30 14:26:43 +0430

    solved the problem in js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const checkButton = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
-checkButton.addEventListener('click', function() {
+checkButton.addEventListener('click', () => {
   const userInput = document.getElementById('text-input').value;
 
   if (userInput.trim().length === 0) {
@@ -11,9 +11,9 @@ checkButton.addEventListener('click', function() {
     const reversedInput = cleanedInput.split('').reverse().join('');
 
     if (cleanedInput === reversedInput) {
-      resultDiv.innerText = userInput + ' is a palindrome';
+      resultDiv.innerText = `${userInput} is a palindrome`;
     } else {
-      resultDiv.innerText = userInput + ' is not a palindrome';
+      resultDiv.innerText = `${userInput} is not a palindrome`;
     }
   }
 });
